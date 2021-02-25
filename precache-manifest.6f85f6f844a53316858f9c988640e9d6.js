804ee3e720417d64a8505faf24e81b971a5b0e01
saranyakannan-v <saranyakannan.v@gmail.com>
2021-02-25 09:26:29 +0100

    Updates

---
+++ precache-manifest.6f85f6f844a53316858f9c988640e9d6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4a46b850fc9c680dd725abc7571b30b",
+    "url": "/React_KeeperApp/index.html"
+  },
+  {
+    "revision": "b4206e2b0966828d36be",
+    "url": "/React_KeeperApp/static/js/2.ee1d7b80.chunk.js"
+  },
+  {
+    "revision": "c760fc5d819448706016",
+    "url": "/React_KeeperApp/static/js/main.b82e93c9.chunk.js"
+  },
+  {
+    "revision": "6bd47b27bb0b19a3bfd9",
+    "url": "/React_KeeperApp/static/js/runtime-main.b95806dd.js"
+  }
+]);
